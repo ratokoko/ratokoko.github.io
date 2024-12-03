e8b31c0dfe764d04da89d157329b6a17a5e67c06
zach <44985939+ratokoko@users.noreply.github.com>
2024-12-03 10:41:29 -0500

    Delete LOB_SKAT directory

--- LOB_SKAT/logic/dropdown.js
+++
@@ -1,9 +0,0 @@
-function getDropdown() {
-    document.getElementById("mainWindow").style.display = 'none';
-    document.getElementById("dropdown-links").style.display = 'block';
-}
-
-function closeDropdown() {
-    document.getElementById("mainWindow").style.display = 'block';
-    document.getElementById("dropdown-links").style.display = 'none';
-}
