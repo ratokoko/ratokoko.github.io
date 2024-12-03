7bae7acad5b7825bdd8e17bf9aa78c29e92c50e8
zach <44985939+ratokoko@users.noreply.github.com>
2024-12-02 21:45:50 -0500

    lob skat test

---
+++ LOB_SKAT/logic/dropdown.js
@@ -0,0 +1,9 @@
+function getDropdown() {
+    document.getElementById("mainWindow").style.display = 'none';
+    document.getElementById("dropdown-links").style.display = 'block';
+}
+
+function closeDropdown() {
+    document.getElementById("mainWindow").style.display = 'block';
+    document.getElementById("dropdown-links").style.display = 'none';
+}
